65f4edae9d6e45bae23ee06a2bb6a6b2b8863a37
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-08-21 10:06:15 +0000

    lint

--- a2aclient/client.go
+++ a2aclient/client.go
@@ -1,3 +1,17 @@
+// Copyright 2025 The A2A Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package a2aclient
 
 import (
@@ -25,7 +39,7 @@ type EventCallback func(ctx context.Context, card a2a.AgentCard, event a2a.Event
 // CallInterceptors are applied before and after every protocol call.
 // EventCallback are notified about new Tasks, Messages and Task update events.
 type Client struct {
-	config       Config
+	Config       Config
 	transport    Transport
 	interceptors []CallInterceptor
 	callbacks    []EventCallback
