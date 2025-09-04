613fd3460f96c0558a50f27662b10288c55b295c
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-09-04 10:41:16 +0000

    task update logic

---
+++ internal/taskupdate/manager.go
@@ -0,0 +1,95 @@
+package taskupdate
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/a2aproject/a2a-go/a2a"
+)
+
+type Saver interface {
+	Save(ctx context.Context, task *a2a.Task) error
+}
+
+type Manager struct {
+	Task  *a2a.Task
+	saver Saver
+}
+
+func NewManager(saver Saver, task *a2a.Task) *Manager {
+	return &Manager{Task: task, saver: saver}
+}
+
+func (ep *Manager) Process(ctx context.Context, event a2a.Event) error {
+	if _, ok := event.(*a2a.Message); ok {
+		return nil
+	}
+
+	if ep.Task == nil {
+		return fmt.Errorf("event processor Task not set")
+	}
+
+	switch v := event.(type) {
+	case *a2a.Task:
+		if err := ep.validate(v.ID, v.ContextID); err != nil {
+			return err
+		}
+		if err := ep.saver.Save(ctx, v); err != nil {
+			return err
+		}
+		ep.Task = v
+		return nil
+
+	case *a2a.TaskArtifactUpdateEvent:
+		if err := ep.validate(v.TaskID, v.ContextID); err != nil {
+			return err
+		}
+		return ep.updateArtifact(ctx, v)
+
+	case *a2a.TaskStatusUpdateEvent:
+		if err := ep.validate(v.TaskID, v.ContextID); err != nil {
+			return err
+		}
+		return ep.updateStatus(ctx, v)
+
+	default:
+		return fmt.Errorf("unexpected event type")
+	}
+}
+
+func (ep *Manager) updateArtifact(_ context.Context, _ *a2a.TaskArtifactUpdateEvent) error {
+	return fmt.Errorf("not implemented")
+}
+
+func (ep *Manager) updateStatus(ctx context.Context, event *a2a.TaskStatusUpdateEvent) error {
+	task := ep.Task
+
+	if task.Status.Message != nil {
+		task.History = append(task.History, *task.Status.Message)
+	}
+
+	if event.Metadata != nil {
+		if task.Metadata == nil {
+			task.Metadata = make(map[string]any)
+		}
+		for k, v := range event.Metadata {
+			task.Metadata[k] = v
+		}
+	}
+
+	task.Status = event.Status
+
+	return ep.saver.Save(ctx, task)
+}
+
+func (ep *Manager) validate(taskID a2a.TaskID, contextID string) error {
+	if ep.Task.ID != taskID {
+		return fmt.Errorf("task IDs don't match: %s != %s", ep.Task.ID, taskID)
+	}
+
+	if ep.Task.ContextID != contextID {
+		return fmt.Errorf("context IDs don't match: %s != %s", ep.Task.ContextID, contextID)
+	}
+
+	return nil
+}
---
+++ internal/taskupdate/task.go
@@ -0,0 +1,20 @@
+package taskupdate
+
+import "github.com/a2aproject/a2a-go/a2a"
+
+func NewSubmittedTask(msg *a2a.Message) *a2a.Task {
+	history := make([]a2a.Message, 1)
+	history[0] = *msg
+
+	contextID := msg.ContextID
+	if len(contextID) == 0 {
+		contextID = a2a.NewContextID()
+	}
+
+	return &a2a.Task{
+		ID:        a2a.NewTaskID(),
+		ContextID: contextID,
+		Status:    a2a.TaskStatus{State: a2a.TaskStateSubmitted},
+		History:   history,
+	}
+}
