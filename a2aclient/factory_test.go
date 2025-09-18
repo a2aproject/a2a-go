57fdf68a07afb90848bc97c1ef5eb2e116e5a361
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-09-18 09:31:02 +0000

    lint

--- a2aclient/factory_test.go
+++ a2aclient/factory_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025 The A2A Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package a2aclient
 
 import (
@@ -165,7 +179,7 @@ func TestFactory_TransportSelection(t *testing.T) {
 				t.Fatalf("expected CreateFromCard() to select %q, got %q", tc.want, selectedProtcol)
 			}
 
-			// CreateFromURL
+			// CreateFromEndpoints
 			selectedProtcol = ""
 			_, err = factory.CreateFromEndpoints(ctx, makeEndpoints(tc.serverSupports))
 			if err != nil && !tc.wantErr {
