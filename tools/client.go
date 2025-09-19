cf9d509bd2b56a010e57e1bcd773e27ccedd9774
Yaroslav Shevchuk <yarolegovich@gmail.com>
2025-09-19 09:58:22 +0000

    tiny client util

--- tools/client.go
+++ tools/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -9,6 +10,8 @@ import (
 	"github.com/a2aproject/a2a-go/a2a"
 	"github.com/a2aproject/a2a-go/a2aclient"
 	"github.com/a2aproject/a2a-go/a2aclient/agentcard"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -29,7 +32,7 @@ func main() {
 		}
 	case "send":
 		if err := handleSend(ctx, os.Args[2:]); err != nil {
-			fmt.Println("Usage: cli send --text {text} [--grpc] {url}")
+			fmt.Println("Usage: cli send --text {text} [--grpc] [--no-stream] {url}")
 			fmt.Printf("Failed with: %v\n", err)
 			os.Exit(1)
 		}
@@ -66,7 +69,15 @@ func handleCard(ctx context.Context, args []string) error {
 		card, err = resolver.Resolve(ctx, agentcard.WithPath(*path))
 	}
 
-	fmt.Println(card)
+	if err != nil {
+		return err
+	}
+
+	jbytes, err := json.MarshalIndent(card, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(jbytes))
 
 	return nil
 }
@@ -75,7 +86,8 @@ func handleSend(ctx context.Context, args []string) error {
 	sendFlags := flag.NewFlagSet("send", flag.ExitOnError)
 
 	text := sendFlags.String("text", "", "Text to send (required)")
-	grpc := sendFlags.Bool("grpc", false, "Use gRPC protocol")
+	useGRPC := sendFlags.Bool("grpc", false, "Use gRPC protocol")
+	noStream := sendFlags.Bool("no-stream", false, "Use gRPC protocol")
 
 	err := sendFlags.Parse(args)
 	if err != nil {
@@ -88,40 +100,93 @@ func handleSend(ctx context.Context, args []string) error {
 	}
 	url := remaining[0]
 
-	var client *a2aclient.Client
-	if *grpc {
-		client, err = createClientFromCard(ctx, url)
-	} else {
-		client, err = createClientGRPC(ctx, url)
-	}
+	client, err := createClient(ctx, url, *useGRPC)
 	if err != nil {
 		return fmt.Errorf("client creation failed: %w", err)
 	}
 
 	msg := a2a.NewMessage(a2a.MessageRoleUser, a2a.TextPart{Text: *text})
-	resp, err := client.SendMessage(ctx, &a2a.MessageSendParams{Message: msg})
+	if *noStream {
+		return sendMessage(ctx, client, msg)
+	}
+
+	return sendStreamingMessage(ctx, client, msg)
+}
+
+func sendMessage(ctx context.Context, client *a2aclient.Client, msg *a2a.Message) error {
+	req := &a2a.MessageSendParams{Message: msg, Config: &a2a.MessageSendConfig{Blocking: true}}
+	resp, err := client.SendMessage(ctx, req)
 	if err != nil {
 		return err
 	}
-
-	fmt.Println(resp)
-
+	if err := printResult(resp); err != nil {
+		return err
+	}
 	return nil
 }
 
-func createClientFromCard(ctx context.Context, url string) (*a2aclient.Client, error) {
-	factory := a2aclient.NewFactory()
-	resolver := agentcard.Resolver{BaseURL: url}
-	card, err := resolver.Resolve(ctx)
-	if err != nil {
-		return nil, err
+func sendStreamingMessage(ctx context.Context, client *a2aclient.Client, msg *a2a.Message) error {
+	for resp, err := range client.SendStreamingMessage(ctx, &a2a.MessageSendParams{Message: msg}) {
+		if err != nil {
+			return err
+		}
+
+		if update, ok := resp.(*a2a.TaskStatusUpdateEvent); ok {
+			if text, ok := makeShortUpdateText(update); ok {
+				fmt.Println(text)
+				continue
+			}
+		}
+
+		if err := printResult(resp); err != nil {
+			return err
+		}
 	}
-	return factory.CreateFromCard(ctx, card)
+	return nil
 }
 
-func createClientGRPC(ctx context.Context, url string) (*a2aclient.Client, error) {
-	factory := a2aclient.NewFactory()
-	return factory.CreateFromEndpoints(ctx, []a2a.AgentInterface{{Transport: a2a.TransportProtocolGRPC, URL: url}})
+func makeShortUpdateText(update *a2a.TaskStatusUpdateEvent) (string, bool) {
+	if update.Status.Message == nil {
+		return fmt.Sprintf("[status=%s]", update.Status.State), true
+	}
+
+	if len(update.Status.Message.Parts) != 1 {
+		return "", false
+	}
+
+	part, ok := update.Status.Message.Parts[0].(a2a.TextPart)
+	if !ok {
+		return "", false
+	}
+
+	return fmt.Sprintf("[status=%s] message: %s", update.Status.State, part.Text), true
+}
+
+func printResult(result any) error {
+	bytes, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(bytes))
+	return nil
+}
+
+func createClient(ctx context.Context, url string, useGRPC bool) (*a2aclient.Client, error) {
+	if useGRPC {
+		creds := grpc.WithTransportCredentials(insecure.NewCredentials())
+		return a2aclient.CreateFromEndpoints(
+			ctx,
+			[]a2a.AgentInterface{{Transport: a2a.TransportProtocolGRPC, URL: url}},
+			a2aclient.WithGRPCTransport(creds),
+		)
+	} else {
+		resolver := agentcard.Resolver{BaseURL: url}
+		card, err := resolver.Resolve(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return a2aclient.CreateFromCard(ctx, card)
+	}
 }
 
 func printHelp() {
@@ -132,13 +197,13 @@ func printHelp() {
 		"  cli <command> [flags] url",
 		"",
 		"COMMANDS:",
-		"  card     Process a card for the given URL",
-		"  send     Send text to a URL with optional gRPC",
+		"  card     Fetch a card from the provided URL",
+		"  send     Send text to an A2A server with the provided URL",
 		"  help     Show this help message",
 		"",
 		"EXAMPLES:",
 		"  cli card https://example.com",
 		"  cli send --text 'Hello World' https://api.example.com",
-		"  cli send --text 'Hello --grpc https://grpc.example.com",
+		"  cli send --text 'Hello' --grpc https://grpc.example.com",
 	)
 }
