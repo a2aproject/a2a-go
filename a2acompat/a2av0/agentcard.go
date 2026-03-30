8de0f1065f381b8d5a7f7586879ba05da271d185
Serob Nahapetyan <serob@google.com>
2026-03-30 15:12:52 +0000

    fix httpAuthSecurityScheme

--- a2acompat/a2av0/agentcard.go
+++ a2acompat/a2av0/agentcard.go
@@ -371,7 +371,7 @@ func (s *namedSecuritySchemes) UnmarshalJSON(b []byte) error {
 		type schemeType struct {
 			Type          string                           `json:"type"`
 			ApiKey        *a2a.APIKeySecurityScheme        `json:"apiKeySecurityScheme,omitempty"`
-			HTTP          *a2a.HTTPAuthSecurityScheme      `json:"httpSecurityScheme,omitempty"`
+			HTTP          *a2a.HTTPAuthSecurityScheme      `json:"httpAuthSecurityScheme,omitempty"`
 			MutualTLS     *a2a.MutualTLSSecurityScheme     `json:"mtlsSecurityScheme,omitempty"`
 			OAuth2        *a2a.OAuth2SecurityScheme        `json:"oauth2SecurityScheme,omitempty"`
 			OpenIDConnect *a2a.OpenIDConnectSecurityScheme `json:"openIdConnectSecurityScheme,omitempty"`
